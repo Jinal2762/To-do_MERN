f20c25d858592712e50b46e4b5b7862b0726c3ee
jinal <jinalthakor27@gmail.com>
2023-05-13 12:50:27 +0530

    initial commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <>
+            <h1 style={{ color: "orangedRed", fontSize: "3em", fontFamily: "cursive" }}>Jinal Thakor</h1>
+            <h1 style={{ color: "orangedRed", fontSize: "3em", fontFamily: "cursive" }}>202212056</h1>
+        </>
+    )
+}
+
+export default About;
